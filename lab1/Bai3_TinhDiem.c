5fc340bce2592fb2d307c532d19383a9d8c9bd3c
voviet49123-lab <voviet49123@gmail.com>
2026-07-02 17:01:20 +0700

    Hoan Thanh Lab1 Bai 3

---
+++ lab1/Bai3_TinhDiem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    char mssv[] = "PS49960";
+    char HoTen[] = "Vo Nguyen Quoc Viet";
+    char t,l,h;
+    float tb;
+    t = 9;
+    l = 7;
+    h = 6;
+    tb = 1.0*(t*2+l+h)/4;
+    printf("Ma so sinh vien: %s\n", mssv);
+    printf("Ho va Ten: %s\n", HoTen);
+    printf("Diem Trung Binh: %.1f\n", tb);
+    return 0;
+}
