932b35315dde1f1d00bc96977c8d9c296a0d020e
voviet49123-lab <voviet49123@gmail.com>
2026-07-03 21:10:49 +0700

    video cam nhan

--- lab1/Bai2_NhapXuat.c
+++ lab1/Bai2_NhapXuat.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main() {    
     
     char mssv[] = "PS49960";
     char hoTen[] = "Vo Nguyen Quoc Viet";
     int namSinh = 1997;
     int tuoi;
-    float diemTB = 8.5;
+    float diemTB = 8.5000;
 
     
     tuoi = 2026 - namSinh;
 
    
     printf("Ma so sinh vien: %s\n", mssv);
-    printf("Ho Va Ten: %s\n", hoTen);
+    printf("Ho va Ten: %s\n", hoTen);
     printf("Nam sinh: %d\n", namSinh);
     printf("Tuoi: %d\n", tuoi);
     printf("Diem Trung Binh: %.1f\n", diemTB);
