900b26e341101768c2ca07e957d150c855b31c21
voviet49123-lab <voviet49123@gmail.com>
2026-07-14 10:14:52 +0700

    14_07_26

--- Nhap_xuat_14_07_26.c
+++ Nhap_xuat_14_07_26.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main(){
 
-    int namSinh;
+    int namSinh,namHientai;
     printf("Hay nhap nam sinh cua nguoi yeu ban: ");
     scanf("%d", &namSinh);
-    printf("Nam sinh cua nguoi yeu ban la %d\nNam nay co ay %d tuoi\n", namSinh,2026-namSinh);
+    printf("Ban dang song vao nam bao nhieu: ");
+    scanf("%d", &namHientai);
+    printf("Nam sinh cua nguoi yeu ban la %d\nNam nay co ay %d tuoi\n", namSinh,namHientai-namSinh);
     return 0;
 }
