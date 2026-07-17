73032cc4af601e81eceb6afd92d76458333af525
voviet49123-lab <voviet49123@gmail.com>
2026-07-17 23:56:34 +0700

    Hoan thanh lab2 bai 1

--- Lab 2/lab2.c
+++ Lab 2/lab2.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #define PI 3.14159
 int main(){
-    int dai,rong,r;
+    float dai,rong,r;
     printf("Nhap chieu dai: ");
-    scanf("%d", &dai);
+    scanf("%f", &dai);
     printf("Nhap chieu rong: ");
-    scanf("%d", &rong);
-    printf("Chu vi la: %d\nDien tich la: %d\n", (dai+rong)*2, dai*rong);
+    scanf("%f", &rong);
+    printf("Chu vi la: %.1f\nDien tich la: %.1f\n", (dai+rong)*2, dai*rong);
 
     printf("Nhap ban kinh hinh tron: ");
-    scanf("%d", &r);
+    scanf("%f", &r);
     float cv= r*2*PI;
     float dt= r*r*PI;
-    printf("Ban kinh: %d\nChu vi: %.1f\nDien tich: %.1f", r,cv,dt);
+    printf("Ban kinh: %f\nChu vi: %.1f\nDien tich: %.1f", r,cv,dt);//chu vi: %f=> 1.2
 
     return 0;
 }
